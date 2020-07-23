c1f19dc79d30feb0678ee33ac93afe733bb2b072
Jrez300 <Joseperez940@live.com>
2020-07-23 16:03:52 -0400

    connected todo input to api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const api = express();
-
 api.use(express.static(__dirname + '/public'));
-
+api.use(bodyParser.json());
 
 api.listen(3000, () => {
   console.log('API up and running!');
@@ -13,4 +14,7 @@ api.listen(3000, () => {
 //     res.send('Hello, world!');
 //   });
 
- 
+api.post('/add', (req, res) => {
+  console.log(req.body);
+  res.send('It works!');
+ });
