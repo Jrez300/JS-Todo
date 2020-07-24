efd6ac8b2b5711d4a577c0edd361baeeda550d06
Jrez300 <Joseperez940@live.com>
2020-07-24 15:21:23 -0400

    connected server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 
 const api = express();
 api.use(express.static(__dirname + '/public'));
 api.use(bodyParser.json());
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Pumas940',
+  database: 'todo' //dont have this yet
+ });
+
+ try {
+  connection.connect();
+ } catch (e) {
+  console.log('Oops. Connection to MySQL failed.');
+  console.log(e);
+ }
 
 api.listen(3000, () => {
   console.log('API up and running!');
@@ -16,5 +31,12 @@ api.listen(3000, () => {
 
 api.post('/add', (req, res) => {
   console.log(req.body);
-  res.send('It works!');
+  
+  connection.query('INSERT INTO tasks (description) VALUES (?)', [req.body.item], (error, results) => {
+   if (error) return res.json({ error: error });
+ connection.query('SELECT LAST_INSERT_ID() FROM tasks', (error, results) => {
+    if (error) return res.json({ error: error });
+ console.log(results);
+   });
+  });
  });
